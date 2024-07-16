f881958588e0d72a58ca590b1d656966375df961
Yash Agarwal <yashag0910@gmail.com>
2024-07-16 14:48:39 +0530

    TopBar Updation

--- frontend/src/components/TopBar.jsx
+++ frontend/src/components/TopBar.jsx
@@ -4,32 +4,100 @@ export const TopBar = () => {
   const [selectedIcon, setSelectedIcon] = useState(1);
 
   const icons = [
-    { id: 1, jsx: (<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-5">
-      <path fillRule="evenodd" d="M12 1.5a.75.75 0 0 1 .75.75V4.5a.75.75 0 0 1-1.5 0V2.25A.75.75 0 0 1 12 1.5ZM5.636 4.136a.75.75 0 0 1 1.06 0l1.592 1.591a.75.75 0 0 1-1.061 1.06l-1.591-1.59a.75.75 0 0 1 0-1.061Zm12.728 0a.75.75 0 0 1 0 1.06l-1.591 1.592a.75.75 0 0 1-1.06-1.061l1.59-1.591a.75.75 0 0 1 1.061 0Zm-6.816 4.496a.75.75 0 0 1 .82.311l5.228 7.917a.75.75 0 0 1-.777 1.148l-2.097-.43 1.045 3.9a.75.75 0 0 1-1.45.388l-1.044-3.899-1.601 1.42a.75.75 0 0 1-1.247-.606l.569-9.47a.75.75 0 0 1 .554-.68ZM3 10.5a.75.75 0 0 1 .75-.75H6a.75.75 0 0 1 0 1.5H3.75A.75.75 0 0 1 3 10.5Zm14.25 0a.75.75 0 0 1 .75-.75h2.25a.75.75 0 0 1 0 1.5H18a.75.75 0 0 1-.75-.75Zm-8.962 3.712a.75.75 0 0 1 0 1.061l-1.591 1.591a.75.75 0 1 1-1.061-1.06l1.591-1.592a.75.75 0 0 1 1.06 0Z" clipRule="evenodd" />
-    </svg>
+    { id: 1, jsx: (<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-4">
+      <path fillRule="evenodd" d="M5.25 6.31v9.44a.75.75 0 0 1-1.5 0V4.5a.75.75 0 0 1 .75-.75h11.25a.75.75 0 0 1 0 1.5H6.31l13.72 13.72a.75.75 0 1 1-1.06 1.06L5.25 6.31Z" clipRule="evenodd" />
+    </svg>    
     ) }, // Up Arrow
-    { id: 2, jsx: (<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-5">
-      <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 7.125C2.25 6.504 2.754 6 3.375 6h6c.621 0 1.125.504 1.125 1.125v3.75c0 .621-.504 1.125-1.125 1.125h-6a1.125 1.125 0 0 1-1.125-1.125v-3.75ZM14.25 8.625c0-.621.504-1.125 1.125-1.125h5.25c.621 0 1.125.504 1.125 1.125v8.25c0 .621-.504 1.125-1.125 1.125h-5.25a1.125 1.125 0 0 1-1.125-1.125v-8.25ZM3.75 16.125c0-.621.504-1.125 1.125-1.125h5.25c.621 0 1.125.504 1.125 1.125v2.25c0 .621-.504 1.125-1.125 1.125h-5.25a1.125 1.125 0 0 1-1.125-1.125v-2.25Z" />
+    { id: 2, jsx: (<svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="none" 
+      strokeWidth={1.}
+      stroke="currentColor"
+      className="size-5"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M5.25 7.5A2.25 2.25 0 0 1 7.5 5.25h9a2.25 2.25 0 0 1 2.25 2.25v9a2.25 2.25 0 0 1-2.25 2.25h-9a2.25 2.25 0 0 1-2.25-2.25v-9Z"
+      />
     </svg>
+    
     ) }, // Square
-    { id: 3, jsx: (<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="w-4 h-4"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 2l5.5 9.5H6.5L12 2z" /></svg>) }, // Diamond
+    { id: 3, jsx: (<svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 256 256"
+      className="w-4 h-4"
+    >
+      <g
+        style={{
+          stroke: 'none',
+          strokeWidth: 0,
+          strokeDasharray: 'none',
+          strokeLinecap: 'butt',
+          strokeLinejoin: 'miter',
+          strokeMiterlimit: 10,
+          fill: 'none',
+          fillRule: 'nonzero',
+          opacity: 1,
+        }}
+        transform="translate(1.407 1.407) scale(2.81 2.81)"
+      >
+        <path
+          d="M 90 82.471 H 0 L 45 4.529 L 90 82.471 z M 9.909 76.75 h 70.182 L 45 15.971 L 9.909 76.75 z"
+          style={{
+            stroke: 'none',
+            strokeWidth: 1,
+            strokeDasharray: 'none',
+            strokeLinecap: 'butt',
+            strokeLinejoin: 'miter',
+            strokeMiterlimit: 10,
+            fill: 'rgb(0,0,0)',
+            fillRule: 'nonzero',
+            opacity: 1,
+          }}
+          transform="matrix(1 0 0 1 0 0)"
+          strokeLinecap="round"
+        />
+      </g>
+    </svg>) }, // Diamond
     { id: 4, jsx: (<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="w-4 h-4">
       <circle cx="12" cy="12" r="10" />
     </svg>) }, // Circle
-    { id: 5, jsx: (<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.25} stroke="currentColor" className="size-5">
+    { id: 5, jsx: (<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.} stroke="currentColor" className="size-5">
         <path strokeLinecap="round" strokeLinejoin="round" d="M17.25 8.25 21 12m0 0-3.75 3.75M21 12H3" />
       </svg>
       ) }, // Right Arrow
-    { id: 6, jsx: (<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-5">
+    { id: 6, jsx: (<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.} stroke="currentColor" className="size-5">
       <path strokeLinecap="round" strokeLinejoin="round" d="M5 12h14" />
     </svg>
     ) }, // Minus
-    { id: 7, jsx: (<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="w-4 h-4"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M16.862 4.487l2.65 2.65-12.4 12.4-3.225.358a1.125 1.125 0 01-1.259-1.26l.358-3.224 12.4-12.4 1.476 1.476z" /><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M11.25 6.75L17.25 12.75" /></svg>) }, // Pencil
-    { id: 8, jsx: (<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-4">
-      <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 6.75 12 3m0 0 3.75 3.75M12 3v18" />
+    { id: 7, jsx: (<svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      className="w-4 h-4"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth={1.25}
+        d="M16.862 4.487l2.65 2.65-12.4 12.4-3.225.358a1.125 1.125 0 01-1.259-1.26l.358-3.224 12.4-12.4 1.476 1.476z"
+      />
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth={2}
+        d="M11.25 6.75L17.25 12.75"
+      />
+    </svg>) }, // Pencil
+    { id: 8, jsx: (<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.} stroke="currentColor" className="size-5">
+      <path strokeLinecap="round" strokeLinejoin="round" d="m10.5 21 5.25-11.25L21 21m-9-3h7.5M3 5.621a48.474 48.474 0 0 1 6-.371m0 0c1.12 0 2.233.038 3.334.114M9 5.25V3m3.334 2.364C11.176 10.658 7.69 15.08 3 17.502m9.334-12.138c.896.061 1.785.147 2.666.257m-4.589 8.495a18.023 18.023 0 0 1-3.827-5.802" />
     </svg>
+    
     ) }, // Academic Cap
-    { id: 9, jsx: (<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-4">
+    { id: 9, jsx: (<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.25} stroke="currentColor" className="size-4">
       <path strokeLinecap="round" strokeLinejoin="round" d="m2.25 15.75 5.159-5.159a2.25 2.25 0 0 1 3.182 0l5.159 5.159m-1.5-1.5 1.409-1.409a2.25 2.25 0 0 1 3.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 0 0 1.5-1.5V6a1.5 1.5 0 0 0-1.5-1.5H3.75A1.5 1.5 0 0 0 2.25 6v12a1.5 1.5 0 0 0 1.5 1.5Zm10.5-11.25h.008v.008h-.008V8.25Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z" />
     </svg>
     ) }, // Photograph
@@ -54,20 +122,27 @@ export const TopBar = () => {
 
   return (
     <div className="flex justify-center">
-      <div className="bg-white border-1 rounded-lg p-4 shadow w-84 h-16">
-        <div className="flex space-x-2">
-          {icons.map((icon) => (
-            <button
-              key={icon.id}
-              onClick={() => setSelectedIcon(icon.id)}
-              className={`icon p-2 cursor-pointer  ${selectedIcon === icon.id ? 'bg-gray-200 rounded border-r' : ''}`}
-            >
-              {icon.jsx}
-            </button>
-          ))}
-        </div>
-      </div>
+  <div className="bg-white border-1 rounded-lg py-1 px-2 shadow w-84 h-13">
+    <div className="flex space-x-1">
+      {icons.map((icon) => (
+        <button
+          key={icon.id}
+          onClick={() => setSelectedIcon(icon.id)}
+          className={`icon p-3 cursor-pointer ${selectedIcon === icon.id ? 'bg-purple-200 rounded border-r ' : ''}`}
+        >
+          <span className="relative">
+            {icon.jsx}
+            {/* Example subscript */}
+            <sub className="absolute text-xs -bottom-2 -right-2 text-gray-400">
+              {icon.id}
+            </sub>
+          </span>
+        </button>
+      ))}
     </div>
+  </div>
+</div>
+
   );
 };
 
